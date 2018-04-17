c3ae986aa9e25d2492d53a8459060f0497014f81
gusmartin <gustavo_martin_02@hotmail.com>
2018-04-17 10:33:56 -0500

    Update tclient.c

--- 7_Sockets/TCP/tclient.c
+++ 7_Sockets/TCP/tclient.c
@@ -18,7 +18,7 @@
 int main(int argc, char ** argv) {
     struct sockaddr_in address;
     struct in_addr inaddr;
-    struct hostent * host;
+    struct hostent * host; //pointer that is aquired when you access the DB
     int sock;
 
     if (argc != 2) {
@@ -30,9 +30,9 @@ int main(int argc, char ** argv) {
        to look it up in DNS. */
     if (inet_aton(argv[1], &inaddr))
         host = gethostbyaddr((char *) &inaddr, sizeof(inaddr), 
-                               AF_INET);
+                               AF_INET);  // from ascii to big endian format gethostbyaddr, then return //host has hostmane, ip and aliases, with afinet you tell the that its ipv4
     else
-        host = gethostbyname(argv[1]);
+        host = gethostbyname(argv[1]); //if ip not converted, seacrh by name
 
     if (!host) {
 	/* We can't find an IP number */
@@ -40,11 +40,11 @@ int main(int argc, char ** argv) {
 	exit(1);
     }
 
-    if ((sock = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+    if ((sock = socket(PF_INET, SOCK_STREAM, 0)) < 0) //0 is defaut protocol
         die("socket");
 
-    address.sin_family = AF_INET;
-    address.sin_port = htons(1234);
+    address.sin_family = AF_INET; //
+    address.sin_port = htons(1234); //set port, in big endian format
 
     /* Take the first IP address associated with this hostname */
     memcpy(&address.sin_addr, host->h_addr_list[0],
@@ -53,9 +53,9 @@ int main(int argc, char ** argv) {
     if (connect(sock, (struct sockaddr *) &address, sizeof(address)))
         die("connect");
 
-    copyData(0, sock);
+    copyData(0, sock); //STDIN to socket, until end of file
 
-    close(sock);
+    close(sock); 
     
     return 0;
 }
